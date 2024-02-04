56a8edceaea7786334405fe55a4df4d5397fbf6c
VolodiaTutko <volodatutko32@gmail.com>
2024-02-04 15:37:12 +0200

    Add project files.

---
+++ Web-MVC-project/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Web_MVC_project.Data
+{
+    public class AppDbContext : DbContext   
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
---
+++ Web-MVC-project/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_MVC_project.Models
+{
+    public class Category
+    {
+        [Key]
+        public int categoryID { get; set; }
+
+        [Required]
+        public string categoryName { get; set; }
+
+        public int displayOrder { get; set; }
+    }
+}
