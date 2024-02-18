a9ac92c4397f4ab35cdc4a8c796c2d5b5aba69a4
VolodiaTutko <volodatutko32@gmail.com>
2024-02-19 00:03:06 +0200

    Add CRUD operations for Category model, add nottification, css styles( bootsrtrap)

--- Web-MVC-project/Controllers/CategoryController.cs
+++ Web-MVC-project/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 using Web_MVC_project.Data;
 using Web_MVC_project.Models;
 
@@ -23,5 +24,136 @@ namespace Web_MVC_project.Controllers
         {
 			return View(new Category());
 		}
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            ValidateCategoryName(obj.categoryName, obj.displayOrder);
+            ValidateUniqueDisplayOrder(obj.displayOrder);
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            SaveCategoryToDatabase(obj);
+
+            TempData["message"] = "The category has been created successfully";
+
+            return RedirectToAction("Index");
+        }
+
+        private void ValidateCategoryName(string categoryName, int displayOrder)
+        {
+            if (Regex.IsMatch(categoryName, @"^\d"))
+            {
+                ModelState.AddModelError("categoryName", "The category name cannot start with a digit");
+            }
+            else if (categoryName == displayOrder.ToString())
+            {
+                ModelState.AddModelError("categoryName", "The category name must be different from the display order");
+            }
+        }
+
+        private void ValidateUniqueDisplayOrder(int displayOrder)
+        {
+            if (_db.Categories.Any(c => c.displayOrder == displayOrder))
+            {
+                ModelState.AddModelError("displayOrder", "The display order must be unique");
+            }
+        }
+
+		public IActionResult Edit(int? categoryId)
+		{
+            if (categoryId == null || categoryId == 0)
+            {
+				return NotFound();
+			}   
+
+            Category? CategoryFromDb = _db.Categories.Find(categoryId);
+            if (CategoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+			return View(CategoryFromDb);
+		}
+
+
+		[HttpPost]
+		public IActionResult Edit(Category obj)
+		{
+            ValidateCategoryName(obj.categoryName, obj.displayOrder);
+            ValidateUniqueDisplayOrder(obj.displayOrder);
+
+            if (!ModelState.IsValid)
+			{
+				return View();
+			}
+
+			UpdateCategoryToDatabase(obj);
+			TempData["message"] = "The category has been edited successfully";
+
+			return RedirectToAction("Index");
+		}
+
+
+		public IActionResult Delete(int? categoryId)
+		{
+			if (categoryId == null || categoryId == 0)
+			{
+				return NotFound();			}
+
+
+			Category? CategoryFromDb = _db.Categories.Find(categoryId);
+			if (CategoryFromDb == null)
+			{
+				return NotFound();
+			}
+
+			return View(CategoryFromDb);
+		}
+
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePOST(int? categoryId)
+		{
+			if (categoryId == null || categoryId == 0)
+			{
+				return NotFound();
+			}
+			Category? CategoryFromDb = _db.Categories.Find(categoryId);
+			if (CategoryFromDb == null)
+			{
+				return NotFound();
+			}
+            DleteCategoryFromDatabase(CategoryFromDb);
+
+			TempData["message"] = "The category has been deleted successfully";
+			return RedirectToAction("Index");
+		}
+
+
+
+		private void DleteCategoryFromDatabase(Category obj)
+		{
+			_db.Categories.Remove(obj);
+			_db.SaveChanges();
+		}
+		private void UpdateCategoryToDatabase(Category obj)
+		{
+			_db.Categories.Update(obj);
+			_db.SaveChanges();
+		}
+
+		private void SaveCategoryToDatabase(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+        }
+          
+
+
+
     }
 }
--- Web-MVC-project/Models/Category.cs
+++ Web-MVC-project/Models/Category.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web_MVC_project.Models
 {
@@ -8,8 +10,12 @@ namespace Web_MVC_project.Models
         public int categoryID { get; set; }
 
         [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
         public string categoryName { get; set; }
-
+        
+        [Range(1, 100, ErrorMessage = "Display Order for category must be between 1-100")]
+        [DisplayName("Display Order")]
         public int displayOrder { get; set; }
     }
 }
